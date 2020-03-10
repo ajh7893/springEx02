27e0c21c6b38f31018185c0ee88dc57d2227fbd4
jonnyAn <ajh7893@gmail.com>
2020-03-10 19:13:18 +0900

    paging

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Select;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 public interface BoardMapper {
+	
+	public List<BoardVO> getListWithPaging(Criteria cri);
 	
 	//@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
